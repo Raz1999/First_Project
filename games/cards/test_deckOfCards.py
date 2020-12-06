9eafecf320bc778a53a0a4a64a8cbe1bfa60a454
Raz1999 <raztzel@gmail.com>
2020-12-06 13:53:33 +0200

    טסטים ראשוניים

--- games/cards/test_deckOfCards.py
+++ games/cards/test_deckOfCards.py
@@ -1,18 +1,29 @@
 from unittest import TestCase
-
+from games.cards.DeckOfCards import DeckOfCards
 
 class TestDeckOfCards(TestCase):
 
-    def setUp(self):
-        self.test_deck
-
+    def test_init(self):
+        deck1 = DeckOfCards()
+        deck2 = DeckOfCards()
 
 
     def test_shuffle_deck(self):
-        self.assertEqual()
+        deck1 = DeckOfCards()
+        self.assertTrue(deck1.shuffle_deck, 'The deck is shuffled')
+
+    def test_shuffle_deck1(self):
+        deck1 = DeckOfCards()
+        deck2 = DeckOfCards()
+
 
     def test_deal_one(self):
-        self.fail()
+        deck1 = DeckOfCards()
+        print(deck1.deal_one())
+        self.assertTrue(len(deck1.deck),51)
+
 
     def test_show(self):
-        self.fail()
+        deck1 = DeckOfCards()
+        check = print(deck1.deck)
+        self.assertEqual(deck1.show(),check)
